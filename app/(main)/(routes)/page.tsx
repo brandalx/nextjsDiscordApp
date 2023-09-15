257bf348f45d03ab9195158600adccb8d4ffb10a
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-15 20:56:31 +0300

    feat: folder structure

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+// import { cn } from "@/lib/utils";
+// const state = true;
+
+export default function Home() {
+  return <div>Protected route for Clerk Auth</div>;
+}
