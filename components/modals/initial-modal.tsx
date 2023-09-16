074aee7888b203a59d68f28b84e97adf18109e9f
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-16 18:12:48 +0300

    feat: modal dialog layout

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,5 +1,27 @@
 "use-client";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogHeader,
+} from "@/components/ui/dialog";
+
 export const InitialModal = () => {
-  return <div>This is modal block</div>;
+  return (
+    <Dialog open>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6 ">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Edit your server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Edit your server name and image, you can always change it later as
+            well
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 };
