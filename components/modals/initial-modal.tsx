375d3583193cc4fb13f8d266d1f5a3e3ae4cf979
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-16 18:17:37 +0300

    feat: zod validation schema

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,4 +1,7 @@
 "use-client";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 import {
   Dialog,
@@ -7,8 +10,20 @@ import {
   DialogTitle,
   DialogHeader,
 } from "@/components/ui/dialog";
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Server name is required",
+  }),
+  imageUrl: z.string().min(1, { message: "Server image is required" }),
+});
 
 export const InitialModal = () => {
+  const form = useForm({
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
   return (
     <Dialog open>
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
