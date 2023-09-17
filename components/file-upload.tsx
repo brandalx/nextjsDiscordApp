df2658aaee4f61724b5ce0443230c44c1c664e12
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-17 10:30:07 +0300

    feat: allowed domnains, files types recognition

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,18 +1,31 @@
 "use client";
+
+import { X } from "lucide-react";
+import Image from "next/image";
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
 import { error } from "console";
+import { classNames } from "uploadthing/client";
 interface FileUploadProps {
   onChange: (url?: string) => void;
   value: string;
   endpoint: "messageFile" | "serverImage";
 }
 export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  const fileType = value?.split(".").pop();
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="uploadresult" className="rounded-full" />
+      </div>
+    );
+  }
   return (
     <UploadDropzone
       endpoint={endpoint}
       onClientUploadComplete={(res) => {
-        onChange(res?.[0].fileUrl);
+        onChange(res?.[0].url);
       }}
       onUploadError={(error: Error) => {
         console.log(error);
