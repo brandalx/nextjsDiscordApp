89de51220d2ecc86e3cbf0e836c0af9f62aefd06
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-17 16:14:22 +0300

    feat: separator

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -2,6 +2,7 @@ import { currentProfile } from "@/lib/current-profile";
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import NavigtaionAction from "./navigation-actions";
+import { Separator } from "../ui/separator";
 export const NavigationSidebar = async () => {
   const profile = await currentProfile();
   if (!profile) {
@@ -20,6 +21,7 @@ export const NavigationSidebar = async () => {
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigtaionAction />
+      <Separator className="h-2px bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
     </div>
   );
 };
