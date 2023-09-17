ca346e7512f7ee878cce6633f78fbc3f4ef262d7
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-17 10:19:06 +0300

    feat uploading dropzone works

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,9 +1,22 @@
 "use client";
+import { UploadDropzone } from "@/lib/uploadthing";
+import "@uploadthing/react/styles.css";
+import { error } from "console";
 interface FileUploadProps {
   onChange: (url?: string) => void;
   value: string;
   endpoint: "messageFile" | "serverImage";
 }
 export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
-  return <div>File Upload Component</div>;
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].fileUrl);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
+  );
 };
