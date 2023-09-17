07a3a22ad222acad0f6551e439a425ad43966a5f
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-17 16:18:58 +0300

    feat render all servers available

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import NavigtaionAction from "./navigation-actions";
 import { Separator } from "../ui/separator";
+import { ScrollArea } from "../ui/scroll-area";
 export const NavigationSidebar = async () => {
   const profile = await currentProfile();
   if (!profile) {
@@ -22,6 +23,11 @@ export const NavigationSidebar = async () => {
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigtaionAction />
       <Separator className="h-2px bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id}>{server.name}</div>
+        ))}
+      </ScrollArea>
     </div>
   );
 };
