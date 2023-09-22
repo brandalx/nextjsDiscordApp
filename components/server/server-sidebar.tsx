a8dd838cb91f2f66f13331cae5a885819eb7a8a1
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-22 12:26:53 +0300

    feat: migration prisma file

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -58,7 +58,7 @@ const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
 
   return (
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2b2d31] bg-[#f2f3f56b]">
-      Server Sidebar
+      {/* <ServerHeader server={server} role={role} /> */}
     </div>
   );
 };
