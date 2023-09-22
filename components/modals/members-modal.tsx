ac34b6acac17f8d1f7e25c07f0b024f0764aa36b
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-23 02:17:26 +0300

    feat: members render and type display

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -8,29 +8,48 @@ import {
 } from "@/components/ui/dialog";
 
 import { useModal } from "@/hooks/use-modal-store";
-import { Label } from "../ui/label";
-import { Input } from "../ui/input";
-import { Button } from "../ui/button";
-import { Check, Copy, RefreshCcw } from "lucide-react";
+import { ServerWithMembersWithProfiles } from "@/types";
 
-import { useState } from "react";
-import axios from "axios";
+import { DialogDescription } from "@radix-ui/react-dialog";
+import { ScrollArea } from "../ui/scroll-area";
+import { UserAvatar } from "../user-avatar";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
 
 export const MembersModal = () => {
   const { isOpen, onClose, type, data, onOpen } = useModal();
 
   const isModalOpen = isOpen && type === "members";
-  const { server } = data;
-
+  const { server } = data as { server: ServerWithMembersWithProfiles };
+  const roleIconMap = {
+    GUEST: null,
+    MODERATOR: <ShieldCheck className="h4 w-4 ml-2 text-indigo-500" />,
+    ADMIN: <ShieldAlert className="h-4 w-4 text-rose-500" />,
+  };
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
-      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+      <DialogContent className="bg-white text-black  overflow-hidden">
         <DialogHeader className="pt-8 px-6 ">
           <DialogTitle className="text-2xl text-center font-bold">
-            Invite members
+            Manage Members
           </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            {server?.members?.length} Members
+          </DialogDescription>
         </DialogHeader>
-        <div className="p-6">hello members</div>
+
+        <ScrollArea className="mt-8 max-h-[420px] pr-6">
+          {server?.members?.map((member) => (
+            <div key={member.id} className="flex items-center gap-x-2 mb-6">
+              <UserAvatar src={member.profile.imageUrl} />
+              <div className="flex flex-col gap-y-1">
+                <div className="text-xs font-semibold flex items-center gap-x-1">
+                  {member.profile.name}
+                  {roleIconMap[member.role]}
+                </div>
+              </div>
+            </div>
+          ))}
+        </ScrollArea>
       </DialogContent>
     </Dialog>
   );
