695c4e04530fa8fbdd6b45b2816ea110bec9d654
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-24 16:27:47 +0300

    feat: ServerChannelProps

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import { Channel, MemberRole, Server } from "@prisma/client";
+
+interface ServerChannelProps {
+  channel: Channel;
+  server: Server;
+  role?: MemberRole;
+}
 const ServerChannel = () => {
   return <div>Server Channel</div>;
 };
