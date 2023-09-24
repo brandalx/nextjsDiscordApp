a040aaacc0347341fb4fed2d5bccde95d0ffa8c5
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-24 16:18:18 +0300

    feat: create channel button func in server sec

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -2,6 +2,9 @@
 
 import { ServerWithMembersWithProfiles } from "@/types";
 import { ChannelType, MemberRole } from "@prisma/client";
+import { ActionTooltip } from "../ui/action-tooltip";
+import { Plus } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerSectionProps {
   label: string;
@@ -10,6 +13,7 @@ interface ServerSectionProps {
   channelType?: ChannelType;
   server?: ServerWithMembersWithProfiles;
 }
+
 const ServerSection = ({
   label,
   role,
@@ -17,7 +21,24 @@ const ServerSection = ({
   channelType,
   server,
 }: ServerSectionProps) => {
-  return <div>Server section</div>;
+  const { onOpen } = useModal();
+  return (
+    <div className="flex items-center justify-between py-2">
+      <p className="text-xs uppercase font-semibold text-zinc-500 dark:text-zinc-400">
+        {label}
+      </p>
+      {role !== MemberRole.GUEST && sectionType === "channels" && (
+        <ActionTooltip label="Create Channel" side="top">
+          <button
+            onClick={() => onOpen("createChannel")}
+            className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+          >
+            <Plus className="h-4 w-4" />
+          </button>
+        </ActionTooltip>
+      )}
+    </div>
+  );
 };
 
 export default ServerSection;
