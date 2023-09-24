3825275816386f761933dce808446b006307055c
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-24 14:47:53 +0300

    feat: ServerSearchProps interfave

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,6 +1,20 @@
 "use client";
 
-const ServerSearch = () => {
+interface ServerSearchProps {
+  data: {
+    label: string;
+    type: "channel" | "member";
+    data:
+      | {
+          icon: React.ReactNode;
+          name: string;
+          id: string;
+        }[]
+      | undefined;
+  }[];
+}
+
+const ServerSearch = ({ data }: ServerSearchProps) => {
   return <div>Server search</div>;
 };
 
