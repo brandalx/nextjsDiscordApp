ce89e05f4aad53bcc13930890c6d329171d4ef92
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-26 00:10:12 +0300

    feat: io config

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -1,3 +1,28 @@
 import { Server as NetServer } from "http";
 import { NextApiRequest } from "next";
 import { Server as ServerIO } from "socket.io";
+
+import { typeNextApiResponseServerIo } from "@/types";
+
+export const config = {
+  api: {
+    bodyParse: false,
+  },
+};
+
+const ioHandler = (req: NextApiRequest, res: typeNextApiResponseServerIo) => {
+  if (!res.socket.server.io) {
+    const path = "/api/socket/io";
+    const httpServer: NetServer = res.socket.server as any;
+
+    const io = new ServerIO(httpServer, {
+      path: path,
+      //@ts-ignore
+      addTrailingSlah: false,
+    });
+    res.socket.server.io = io;
+  }
+  res.end();
+};
+
+export default ioHandler;
