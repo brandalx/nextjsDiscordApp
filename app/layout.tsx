b6b136feb479f06693f433ed8f70574b83a3f493
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-26 00:12:57 +0300

    fix: providers presence folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/ui/providers/theme.provider";
-import { ModalProvider } from "@/components/ui/providers/modal-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 import { cn } from "@/lib/utils";
 const font = Open_Sans({ subsets: ["latin"] });
 
