07afe818917a071d6704e5bf00a1d799c1260fa4
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-25 20:50:15 +0300

    feat: channel id search for channel and members

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,42 @@
-const ChannelIdPage = () => {
-  return <div>Channel Id Page</div>;
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ChannelIdProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+const ChannelIdPage = async ({ params }: ChannelIdProps) => {
+  const profile = await currentProfile();
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.server.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
