9ac93a37c45d0c7fdd9b803379803052823048b1
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-26 00:51:28 +0300

    feat: interface for chat input

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -1,6 +1,18 @@
 "use client";
 
-const ChatInput = () => {
+import * as z from "zod";
+
+interface ChatInputProps {
+  apiUrl: string;
+  query: Record<string, any>;
+  type: "conversation" | "channel";
+  name: string;
+}
+
+const formSchema = z.object({
+  content: z.string().min(1),
+});
+const ChatInput = ({ apiUrl, query, type, name }: ChatInputProps) => {
   return <div>ChatInput</div>;
 };
 
