21713f51a06518fe77280007fa97f741227a5714
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-26 00:48:19 +0300

    feat: chat input comp init

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatHeader from "@/components/chat/chat-header";
+import ChatInput from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -40,6 +41,8 @@ const ChannelIdPage = async ({ params }: ChannelIdProps) => {
         serverId={channel.serverId}
         type="channel"
       />
+      <div className="flex-1"> Future Messages</div>
+      <ChatInput />
     </div>
   );
 };
