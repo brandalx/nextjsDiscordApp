f45577e32e49b94b22ce294bc550b4cca4e78662
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-27 22:37:38 +0300

    feat: POST endpoint for messages, validations

--- lib/current-profile-pages.ts
+++ lib/current-profile-pages.ts
@@ -1,7 +1,7 @@
 import { getAuth } from "@clerk/nextjs/server";
 import { db } from "./db";
 import { NextApiRequest } from "next";
-export const currentProfile = async (req: NextApiRequest) => {
+export const currentProfilePages = async (req: NextApiRequest) => {
   const { userId } = getAuth(req);
   if (!userId) {
     return null;
