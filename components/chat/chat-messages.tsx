8f0d62d48dc8d7666c51c5aba258ad1096c7ff85
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-28 00:44:43 +0300

    feat: chat messages init

---
+++ components/chat/chat-messages.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Member } from "@prisma/client";
+
+interface ChatMessagesProps {
+  name: string;
+  member: Member;
+  chatId: string;
+  apiUrl: string;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
+  paramKey: "channelId" | "conversationId";
+  paramValues: string;
+  type: "channel" | "conversation";
+}
+const ChatMessages = () => {
+  return <div>Chat messages</div>;
+};
+
+export default ChatMessages;
