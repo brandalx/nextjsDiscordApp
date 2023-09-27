549738a3b2725830612477d56bf165100f74b44b
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-28 02:07:25 +0300

    feat: footer, header init

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,21 @@
+import Footer from "@/components/auth/footer";
+import Header from "@/components/auth/header";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className=" h-full flex items-center justify-center">{children}</div>
+    <div className="h-full">
+      <div>
+        <Header />
+      </div>
+      <div className="h-full">
+        <div className="h-full flex-col flex items-center justify-center">
+          <div>{children}</div>
+        </div>
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
   );
 };
 
