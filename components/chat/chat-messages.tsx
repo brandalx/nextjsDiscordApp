132efc4ea5bf13304cd2f6c3e856a69a42652d4f
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-28 01:39:54 +0300

    feat: server infinite loaders handlers

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -3,6 +3,7 @@
 import { Member } from "@prisma/client";
 import ChatWelcome from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
+import { Loader2, ServerCrash } from "lucide-react";
 
 interface ChatMessagesProps {
   name: string;
@@ -34,6 +35,30 @@ const ChatMessages = ({
       paramKey,
       paramValue,
     });
+
+  if (status === "loading") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center ">
+        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin" />
+
+        <p className="text-xs text-zinc-500 dark:text-zinc-400 mt-2">
+          Loading messages...
+        </p>
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center ">
+        <ServerCrash className="h-7 w-7 text-zinc-500 " />
+
+        <p className="text-xs text-zinc-500 dark:text-zinc-400 mt-2">
+          Something went wrong, try refresh this page
+        </p>
+      </div>
+    );
+  }
   return (
     <div className="flex-1 flex flex-col py-4 overflow-auto">
       <div className="flex-1" />
