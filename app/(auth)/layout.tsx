1bb07b3caabb5cec6940ddc8800d102ebfab3763
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-28 18:22:50 +0300

    fix: layout fix

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,13 +4,13 @@ import { Fingerprint } from "lucide-react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full">
+    <div className="h-full flex flex-col justify-between">
       <div>
         <Header />
       </div>
 
-      <div className="h-full">
-        <div className="h-full flex-col flex items-center justify-center">
+      <div className="">
+        <div className=" flex-col flex items-center justify-center">
           <div className="flex items-center justify-center my-2">
             <Fingerprint className="text-indigo-500" size={40} />
             <div>
