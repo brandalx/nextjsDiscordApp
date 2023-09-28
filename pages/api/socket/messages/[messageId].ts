b2d2e594a09cd4cd2650eee6f11de57ccd0cc76b
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-28 18:45:34 +0300

    feat: messages logic for server

--- pages/api/socket/messages/[messageId].ts
+++ pages/api/socket/messages/[messageId].ts
@@ -1,5 +1,7 @@
 import { currentProfilePages } from "@/lib/current-profile-pages";
+import { db } from "@/lib/db";
 import { NextApiResponseServerIo } from "@/types";
+import { MemberRole } from "@prisma/client";
 import { NextApiRequest } from "next";
 
 export default async function handler(
@@ -24,6 +26,89 @@ export default async function handler(
 
     if (!channelId) {
       return res.status(401).json({ error: "Channel ID is missing" });
+    }
+
+    const server = await db.server.findFirst({
+      where: {
+        id: serverId as string,
+        members: {
+          some: {
+            profileId: profile.id,
+          },
+        },
+      },
+      include: {
+        members: true,
+      },
+    });
+
+    if (!server) {
+      return res.status(404).json({ error: "Server not found" });
+    }
+
+    const channel = await db.channel.findFirst({
+      where: {
+        id: channelId as string,
+        serverId: serverId as string,
+      },
+    });
+
+    if (!channel) {
+      return res.status(404).json({ error: "Channel not found" });
+    }
+
+    const member = server.members.find(
+      (member) => member.profileId === profile.id
+    );
+    if (!member) {
+      return res.status(404).json({ error: "Channel not found" });
+    }
+
+    let message = await db.message.findFirst({
+      where: {
+        id: messageId as string,
+        channelId: channelId as string,
+      },
+      include: {
+        member: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+
+    if (!message || message.deleted) {
+      return res.status(404).json({ error: "Message not found" });
+    }
+
+    const isMessageOwner = message.memberId === member.id;
+    const isAdmin = member.role === MemberRole.ADMIN;
+    const isModerator = member.role === MemberRole.MODERATOR;
+    const canModify = isMessageOwner || isAdmin || isModerator;
+
+    if (!canModify) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    if (req.method === "DELETE") {
+      message = await db.message.update({
+        where: {
+          id: messageId as string,
+        },
+        data: {
+          fileUrl: null,
+          content: "This message has been deleted",
+          deleted: true,
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+      });
     }
   } catch (error) {
     console.log("[MESSAGE_ID]", error);
