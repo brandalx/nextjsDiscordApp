5c6b94caea71cccc90bfde1cac57233ecbd79315
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-28 19:58:01 +0300

    feat: socket for ind messages custom hook

--- hooks/use-chat-socket.ts
+++ hooks/use-chat-socket.ts
@@ -48,5 +48,33 @@ export const UseChatSocket = ({
         };
       });
     });
-  }, []);
+
+    socket.on(addKey, (message: MessageWithMemberWithProfile) => {
+      queryClient.setQueryData([queryKey], (oldData: any) => {
+        if (!oldData || !oldData.pages || oldData.pages.length === 0) {
+          return {
+            pages: [
+              {
+                items: [message],
+              },
+            ],
+          };
+        }
+        const newData = [...oldData.pages];
+        newData[0] = {
+          ...newData[0],
+          items: [message, newData[0].items],
+        };
+        return {
+          ...oldData,
+          pages: newData,
+        };
+      });
+    });
+
+    return () => {
+      socket.off(addKey);
+      socket.off(updateKey);
+    };
+  }, [queryClient, addKey, queryKey, socket, updateKey]);
 };
