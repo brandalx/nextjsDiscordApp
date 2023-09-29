9dd633a7799ce0b4609fd08fd533c0d8f526770d
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-29 04:28:19 +0300

    feat: live text changed

--- components/socket-indicator.tsx
+++ components/socket-indicator.tsx
@@ -11,14 +11,14 @@ export const SocketIndicator = () => {
   if (!isConnected) {
     return (
       <Badge variant="outline" className="bg-yellow-600 text-white border-none">
-        Fallback: Pooling every 1s
+        Reconnect
       </Badge>
     );
   }
 
   return (
     <Badge variant="outline" className="bg-emerald-600 text-white border-none">
-      Live: Real-time update
+      Live
     </Badge>
   );
 };
