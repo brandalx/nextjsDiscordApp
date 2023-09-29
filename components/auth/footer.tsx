ad6d3de9cd9023d690f65799e6d936b2c59f8f18
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-29 07:05:48 +0300

    update: home screen  ui

--- components/auth/footer.tsx
+++ components/auth/footer.tsx
@@ -4,12 +4,13 @@ import {
   Fingerprint,
   Github,
   Linkedin,
+  Mail,
   Phone,
 } from "lucide-react";
 
 const Footer = () => {
   return (
-    <div className="border border-black dark:border-white py-5 rounded-lg container">
+    <div className="border border-black dark:border-white py-2 border-dashed my-2 rounded-lg container">
       <div className="flex items-center justify-center">
         <Fingerprint className="text-indigo-500" />
         <div>
@@ -18,18 +19,22 @@ const Footer = () => {
       </div>
       <div>
         <div className="justify-center flex items-center">
-          <a href="#">
+          <a href="https://github.com/brandalx">
             <Github className="h-4 w-4 mx-2" />
           </a>
-          <a href="#">
+          <a href="https://linkedin.com/in/brandonolan">
             <Linkedin className="h-4 w-4 mx-2" />
           </a>
           <a href="#">
             <Code2 className="h-4 w-4 mx-2" />
           </a>
 
-          <a href="#">
+          <a href="tel:+972522733369">
             <Phone className="h-4 w-4 mx-2" />
+          </a>
+
+          <a href="mailto:brndalx@gmail.com">
+            <Mail className="h-4 w-4 mx-2" />
           </a>
         </div>
         <a
